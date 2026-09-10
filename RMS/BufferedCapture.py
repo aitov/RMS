e76eff3676a3c1f0cef47fe56627b8f6f7d41a30
alexaitov <aaitov@gmail.com>
2026-09-10 23:31:57 +0300

    Add support in Gts backend devices with v4l2src (RAW MIPI)

--- RMS/BufferedCapture.py
+++ RMS/BufferedCapture.py
@@ -1254,7 +1254,8 @@ class BufferedCapture(Process):
                 storage_branch = (
                     "t. ! queue2 max-size-buffers=150 max-size-bytes=2097152 max-size-time=5000000000 ! "
                     "videoconvert ! video/x-raw,format=I420 ! "
-                    "x264enc speed-preset=superfast pass=qual quantizer=12 bitrate={:d} ! h264parse ! "
+                    "queue max-size-buffers=3 leaky=downstream ! " 
+                    "x264enc speed-preset=ultrafast pass=qual quantizer=12 bitrate={:d} ! h264parse ! "
                     "splitmuxsink name=splitmuxsink0 async-finalize=true max-size-time={:d} muxer-factory=mp4mux"
                     ).format(self.config.raw_video_bitrate, int(segment_duration_sec*1e9))
             else:
